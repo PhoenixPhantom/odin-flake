120e767b89929feb86b2d8f2692f5a3c8bc4a9ab
Phoenix Phantom <raffael.scagnetti@bluewin.ch>
2025-06-10 13:34:43 +0200

    Update to dev-2025-06

--- flake.nix
+++ flake.nix
@@ -16,44 +16,53 @@
       {
          packages.${system}.default =
          let
-            odin-version = "dev-2025-04";
+            odin-version = "dev-2025-06";
             hashes = {
-               "dev-2025-06" = "sha256-Dhy62+ccIjXUL/lK8IQ+vvGEsTrd153tPp4WIdl3rh4="; # currently not compiling for me (gb_fprintf_va can never be inlined ...)
+               "dev-2025-06" = "sha256-EZT2v1uRQQ5+CxiJaCizf1JtmPbCSsz94JAgJXqhQ6s="; # currently not compiling for me (gb_fprintf_va can never be inlined ...)
                "dev-2025-04" = "sha256-dVC7MgaNdgKy3X9OE5ZcNCPnuDwqXszX9iAoUglfz2k=";
                "dev-2025-03" = "sha256-QmbKbhZglucVpsdlyxJsH2bslhqmd0nuMPC+E0dTpiY=";
             };
          in
          stdenv.mkDerivation {
             name = "odin";
-            src = fetchFromGitHub {
-               owner = "odin-lang";
-               repo = "Odin";
-               rev = odin-version;
-               hash = hashes.${odin-version};
+            # src = fetchFromGitHub {
+            #    owner = "odin-lang";
+            #    repo = "Odin";
+            #    rev = odin-version;
+            #    hash = hashes.${odin-version};
+            # };
+
+            src = fetchzip {
+               url = "https://github.com/odin-lang/Odin/releases/download/dev-2025-06/odin-linux-amd64-${odin-version}.zip";
+               sha256 = hashes.${odin-version};
             };
 
-            postPatch = ''
-               patchShebangs --build build_odin.sh
-               '';
-
-            LLVM_CONFIG = lib.getExe' llvmPackages.llvm.dev "llvm-config";
-
-            dontConfigure = true;
-
-            buildFlags = [ "release" ];
-
+            # postPatch = ''
+            #    patchShebangs --build build_odin.sh
+            #    '';
+            #
+            # LLVM_CONFIG = lib.getExe' llvmPackages.llvm.dev "llvm-config";
+            #
+            # dontConfigure = true;
+            #
+            # buildFlags = [ "release" ];
+            #
             nativeBuildInputs = [
-               makeBinaryWrapper which gnumake llvmPackages.clang
+               makeBinaryWrapper which gnumake llvmPackages.clang gnutar
             ];
 
             installPhase = ''
                runHook preInstall
 
+               mkdir -p $out/tmp
+               tar -xzf dist.tar.gz -C $out/tmp --strip-components=1
+
                mkdir -p $out/bin
-               cp odin $out/bin/odin
+               cp $out/tmp/odin $out/bin/odin
 
                mkdir -p $out/share
-               cp -r {base,core,vendor,shared} $out/share
+               cp -r $out/tmp/{base,core,vendor,shared} $out/share
+               rm -r $out/tmp
 
                wrapProgram $out/bin/odin \
                --prefix PATH : ${lib.makeBinPath (with llvmPackages; [
