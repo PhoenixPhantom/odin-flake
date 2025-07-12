96a99c6fddf22d96faf05f9e2c06a3cf2e3e86e5
Phoenix Phantom <raffael.scagnetti@bluewin.ch>
2025-07-12 19:26:12 +0200

    Update to dev-2025-07

--- flake.nix
+++ flake.nix
@@ -4,6 +4,82 @@
       nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
       flake-utils.url = "github:numtide/flake-utils";
    };
+   # until odin reliably builds again
+#    outputs = { self, nixpkgs, flake-utils }:
+#       let
+#          overlays = [];
+#          system = "x86_64-linux";
+#          pkgs = import nixpkgs {
+#             inherit system overlays;
+#          };
+#       in
+#       with pkgs;
+#       {
+#          packages.${system}.default =
+#          let
+#             odin-version = "dev-2025-07";
+#             hashes = {
+#                "dev-2025-07" = "sha256-4jhxvQHirNm4B4Wf5Ak0lhAbwaRw6ajWA0JhIn1NYwM=";
+#                "dev-2025-04" = "sha256-dVC7MgaNdgKy3X9OE5ZcNCPnuDwqXszX9iAoUglfz2k=";
+#                "dev-2025-03" = "sha256-QmbKbhZglucVpsdlyxJsH2bslhqmd0nuMPC+E0dTpiY=";
+#             };
+#          in
+#          stdenv.mkDerivation {
+#             name = "odin";
+#             src = fetchFromGitHub {
+#                owner = "odin-lang";
+#                repo = "Odin";
+#                rev = odin-version;
+#                hash = hashes.${odin-version};
+#             };
+#
+#             nativeBuildInputs = [
+#                makeBinaryWrapper which gnumake llvmPackages_20.clang
+#             ];
+#             postPatch = ''
+#                substituteInPlace build_odin.sh \
+#                --replace-fail '-framework System' '-lSystem'
+#                patchShebangs build_odin.sh
+#             '';
+#
+#             LLVM_CONFIG = "${llvmPackages.llvm.dev}/bin/llvm-config";
+#             dontConfigure = true;
+#             buildFlags = [
+#                "release"
+#             ];
+#
+#             installPhase = ''
+#                runHook preInstall
+#
+#                mkdir -p $out/bin
+#                cp odin $out/bin/odin
+#
+#                mkdir -p $out/share
+#                cp -r {base,core,vendor,shared} $out/share
+#
+#                wrapProgram $out/bin/odin \
+#                --prefix PATH : ${lib.makeBinPath (with llvmPackages; [
+#                      bintools
+#                      llvm
+#                      clang
+#                      lld
+#                ])} \
+#                --set-default ODIN_ROOT $out/share
+#
+#                make -C $out/share/vendor/stb/src
+#
+#                make -C $out/share/vendor/cgltf/src
+#
+#                runHook postInstall
+#                '';
+#          };
+#          overlays = {
+#             default = final: prev: {
+#                odin = self.packages.${prev.system}.default;
+#             };
+#          };
+#       };
+# }
    outputs = { self, nixpkgs, flake-utils }:
       let
          overlays = [];
@@ -16,8 +92,9 @@
       {
          packages.${system}.default =
          let
-            odin-version = "dev-2025-06";
+            odin-version = "dev-2025-07";
             hashes = {
+               "dev-2025-07" = "sha256-n5qqjYUnt9Bo4rV+hNUtqGzKfxePHnz3HOS/solL+Uc="; # just as in 2025-07
                "dev-2025-06" = "sha256-EZT2v1uRQQ5+CxiJaCizf1JtmPbCSsz94JAgJXqhQ6s="; # currently not compiling for me (gb_fprintf_va can never be inlined ...)
                "dev-2025-04" = "sha256-dVC7MgaNdgKy3X9OE5ZcNCPnuDwqXszX9iAoUglfz2k=";
                "dev-2025-03" = "sha256-QmbKbhZglucVpsdlyxJsH2bslhqmd0nuMPC+E0dTpiY=";
@@ -33,7 +110,7 @@
             # };
 
             src = fetchzip {
-               url = "https://github.com/odin-lang/Odin/releases/download/dev-2025-06/odin-linux-amd64-${odin-version}.zip";
+               url = "https://github.com/odin-lang/Odin/releases/download/dev-2025-07/odin-linux-amd64-${odin-version}.tar.gz";
                sha256 = hashes.${odin-version};
             };
 
@@ -54,15 +131,11 @@
             installPhase = ''
                runHook preInstall
 
-               mkdir -p $out/tmp
-               tar -xzf dist.tar.gz -C $out/tmp --strip-components=1
-
                mkdir -p $out/bin
-               cp $out/tmp/odin $out/bin/odin
+               cp odin $out/bin/odin
 
                mkdir -p $out/share
-               cp -r $out/tmp/{base,core,vendor,shared} $out/share
-               rm -r $out/tmp
+               cp -r {base,core,vendor,shared} $out/share
 
                wrapProgram $out/bin/odin \
                --prefix PATH : ${lib.makeBinPath (with llvmPackages; [
